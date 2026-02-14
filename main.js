d3f157951a4dae7b6279f48956ca7716ca1d8436
Anvar <anvarhayrullaev00@gmail.com>
2026-02-14 08:17:24 +0500

    done

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 let i=0;
 (function f(){
-  const h=document.querySelector("h1");
-  h.style.color=`rgb(${i*2},0,0)`;
-  h.textContent=i<100?i++:i+" LEVEL";
+  let h=document.querySelector("h1");
+  h.style.color = i < 50 ? "red" : "green";
+  h.innerText = i < 100 ? i++ : i + " LEVEL";
   setTimeout(f,100);
 })();
+
+
